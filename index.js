f082aec62ae47a2f883f8a9d77344d9bb92ff890
joshua <85379582+joshuaelliot@users.noreply.github.com>
2023-10-08 10:48:54 -0600

    creando el package npm

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+Este paquete se encarga de otorgarte los mejores 
+console.log de tu hermosa vida
+
+*/
+
+ class ConsoleLogsito{
+
+    consoleSad = ()=>{
+        console.log("este es un console.log despues de tumbar el servidor que tenia la planilla de la empresa .");
+
+    };
+    consoleHappy=()=>{
+        console.log("este es un console.log despues de que me depositaran el sueldo de todos por error ! psdt : ya me fui del pais XD.");
+    }
+}
